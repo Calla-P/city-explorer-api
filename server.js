5ee12ba2aef47de7f5a506d353287ed7dc53cec1
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2023-01-28 13:55:17 -0800

    Updates my files

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict'
+
+console.log('My first server');
+
+// require('dotenv').config();
+// // express server library
+// const express = require('express');
+
+
+// const app = express();
+
+// const cors = require ('cors');
+// const { response } = require('express');
+
+// // anyone can request to our server, typically you want o put url limitations
+// app.use(cors())
+// // we typically get the the env with the process, this portion is mostly for trouble shooting
+
+// const PORT =process.env.PORT
+
+// const list = require('./data/weather.json')
+
+// //  request is the request object the response is the object we are sending back
+
+// app.get('./',(request,response) =>{
+//     response.send('Hey  your endpoint is working');
+
+// });
+
+// app.get('/bananas',(request,response)=>{
+//     response.send('This is bananas')
+// });
+
+// app.listen(PORT,() => console.log ('listening on $(PORT)'
+// ));
+
+// app.get('/list',(request,response)=>{
+// response.send(list)});
+
+
+// class List{
+
+//     constructor(type){
+//         let 
+//     }
+// }
+
+// getItems(){
+//     return this.items.map(item =>{
+//         return{item.name, description: item.desription}
+//     }
+//         )
+// }
