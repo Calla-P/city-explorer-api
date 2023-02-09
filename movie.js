5d46c2bceb1581bf04d6c611738fc841a348cc5a
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2023-02-08 20:29:33 -0800

    including the cache still need to test

--- movie.js
+++ movie.js
@@ -1,14 +1,33 @@
 const axios = require('axios');
+let cache = {
+    cachedMovie: null,
+    timedMovieStamp: null,
+    searchedValue:null
+};
 
 let movies = async function(request, response) {
+    //varibles for cache
+    let testTimedCache = 1000 * 60 * 60 * 24 * 7 * 4; //one month
+    let currentTime = Date.now();
+
+    //varibles for query
     let searchMovie = request.query.search;
+    if(cache && currentTime - cache.timedMovieStamp < testTimedCache && cache.searchedValue === searchMovie) {
+        console.log('Stored cache data');
+        response.status(200).send(cache.cachedMovie);
+    } else {
     let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchMovie}`;
     console.log('movieURL', movieURL);
+
     let movieResults = await axios.get(movieURL);
     console.log('movieResults.data', movieResults.data);
 
     let bestMovies = movieResults.data.results.map(movie => new Movie(movie));
     bestMovies.length < 1 ? response.status(500).send('Nope. That is not found here.') : response.status(200).send(bestMovies);
+    cache.cachedMovie = bestMovies;
+    cache.timedMovieStamp = Date.now();
+    cache.searchedValue = searchMovie;
+ };
 };
 
 class Movie {
--- weather.js
+++ weather.js
@@ -1,8 +1,27 @@
 const axios = require('axios');
 
+let cache = {
+    cachedWeather: null,
+    weatherTimestamp: null,
+    searchedCoordinates: {
+        lat: null,
+        lon: null
+    }
+};
+
 let weather = async function(request, response) {
+    //varibles for the cache
+    let testTimedCache = 1000 * 20;
+    let currentTime = Date.now();
+    // varibles for the querry
     let lat = request.query.lat;
     let lon = request.query.lon;
+
+    // is cache present & recent, return cache, else run new API call
+    if(cache && currentTime - cache.weatherTimestamp < testTimedCache && cache.searchedCoordinates[lat] === lat && cache.searchedCoordinates[lon] === lon){
+        console.log('Stored data in cache');
+        response.status(200).send(cache.cachedWeather);
+    } else {
     let weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=10&key=${process.env.WEATHER_API_KEY}`;
 
     let searchQuery = await axios.get(weatherURL);
@@ -14,6 +33,12 @@ let weather = async function(request, response) {
     console.log(threeDayForecast);
 
     threeDayForecast.length < 1 ? response.status(500).send('Error. City not covered by system.') : response.status(200).send(threeDayForecast);
+    cache.cachedWeather = threeDayForecast;
+    cache.weatherTimestamp = Date.now();
+    cache.searchedCoordinates[lat] = lat;
+    cache.searchedCoordinates[lon] = lon;
+
+};
 };
 
 class Forecast {
